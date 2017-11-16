2cc8a20f4eab98d4af8fc5dd8d717d536010d1b2
rusjave <rj@realestate.ph>
2017-11-16 21:52:35 +0800

    Update homepage template

---
+++ application/views/public/_includes/slider.php
@@ -0,0 +1,91 @@
+<section class="section-white">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="fb-page" data-href="https://www.facebook.com/alphameddiagnosticlaboratory/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+                    <blockquote cite="https://www.facebook.com/alphameddiagnosticlaboratory/" class="fb-xfbml-parse-ignore">
+                        <a href="https://www.facebook.com/alphameddiagnosticlaboratory/">Alphamed Diagnostic Laboratory</a>
+                    </blockquote>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                  <!-- Indicators -->
+                  <ol class="carousel-indicators">
+                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="4"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="5"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="6"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="7"></li>
+                  </ol>
+
+                      <!-- Wrapper for slides -->
+                      <div class="carousel-inner">
+                        <div class="item active">
+                          <img src="<?php echo base_url('assets/images/alphamed-slider5.jpg'); ?>"  style="height: 500px; width: 100%;" alt="...">
+                          <div class="carousel-caption">
+                            <h2></h2>
+                          </div>
+                        </div>
+                        <div class="item">
+                        <img src="<?php echo base_url('assets/images/alphamed-slider2.jpg'); ?>"  style="height: 500px; width: 100%;" alt="...">
+                          <div class="carousel-caption">
+                            <h2></h2>
+                          </div>
+                        </div>
+                        <div class="item">
+                        <img src="<?php echo base_url('assets/images/alphamed-slider1.jpg'); ?>"  style="height: 500px; width: 100%;" alt="...">
+                          <div class="carousel-caption">
+                            <h2></h2>
+                          </div>
+                        </div>
+                        <div class="item">
+                          <img src="<?php echo base_url('assets/images/alphamed-slider3.jpg'); ?>"  style="height: 500px; width: 100%;" alt="...">
+                          <div class="carousel-caption">
+                            <h2></h2>
+                          </div>
+                        </div>
+                        <div class="item">
+                          <img src="<?php echo base_url('assets/images/alphamed-slider4.jpg'); ?>"  style="height: 500px; width: 100%;" alt="...">
+                          <div class="carousel-caption">
+                            <h2></h2>
+                          </div>
+                        </div>
+                        <div class="item">
+                          <img src="<?php echo base_url('assets/images/alphamed-slider6.jpg'); ?>"  style="height: 500px; width: 100%;" alt="...">
+                          <div class="carousel-caption">
+                            <h2></h2>
+                          </div>
+                        </div>
+                        <div class="item">
+                          <img src="<?php echo base_url('assets/images/alphamed-slider7.jpg'); ?>"  style="height: 500px; width: 100%;" alt="...">
+                          <div class="carousel-caption">
+                            <h2></h2>
+                          </div>
+                        </div>
+                        <div class="item">
+                          <img src="<?php echo base_url('assets/images/alphamed-slider8.jpg'); ?>"  style="height: 500px; width: 100%;" alt="...">
+                          <div class="carousel-caption">
+                            <h2></h2>
+                          </div>
+                        </div>
+                      </div>
+
+                      <!-- Controls -->
+                      <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left"></span>
+                      </a>
+                      <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right"></span>
+                      </a>
+                    </div>
+                </div>
+            <div class="col-md-3">
+                <img alt="140x140" data-src="holder.js/140x140" class="img-rounded" style="width: 300px; height: 500px;" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgdmlld0JveD0iMCAwIDE0MCAxNDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzE0MHgxNDAKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNWZjNGE0ODE3YyB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1ZmM0YTQ4MTdjIj48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjQzLjUiIHk9Ijc0LjgiPjE0MHgxNDA8L3RleHQ+PC9nPjwvZz48L3N2Zz4=" data-holder-rendered="true">
+            </div>
+        </div>
+    </div>
+</section>
--- application/views/public/home.php
+++ application/views/public/home.php
@@ -1,100 +1,48 @@
 <?php
 	$this->load->view('public/_includes/header');
 ?>
-<section class="section-white">
-  <div class="container">
-    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
-      <ol class="carousel-indicators">
-        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="4"></li>
-      </ol>
 
-      <!-- Wrapper for slides -->
-      <div class="carousel-inner">
-        <div class="item active">
-          <img src="<?php echo base_url('assets/images/alphamed-slider5.jpg'); ?>"  style="height: 450px; width: 100%;" alt="...">
-          <div class="carousel-caption">
-            <h2></h2>
-          </div>
-        </div>
-        <div class="item">
-        <img src="<?php echo base_url('assets/images/alphamed-slider2.jpg'); ?>"  style="height: 450px; width: 100%;" alt="...">
-          <div class="carousel-caption">
-            <h2></h2>
-          </div>
-        </div>
-        <div class="item">
-          <img src="<?php echo base_url('assets/images/alphamed-slider3.jpg'); ?>"  style="height: 450px; width: 100%;" alt="...">
-          <div class="carousel-caption">
-            <h2></h2>
-          </div>
-        </div>
-        <div class="item">
-          <img src="<?php echo base_url('assets/images/alphamed-slider4.jpg'); ?>"  style="height: 450px; width: 100%;" alt="...">
-          <div class="carousel-caption">
-            <h2></h2>
-          </div>
-        </div>
-        <div class="item">
-          <img src="<?php echo base_url('assets/images/alphamed-slider6.jpg'); ?>"  style="height: 450px; width: 100%;" alt="...">
-          <div class="carousel-caption">
-            <h2></h2>
-          </div>
-        </div>
-      </div>
-
-      <!-- Controls -->
-      <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
-      </a>
-      <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
-      </a>
-    </div>
-
-  </div>
-</section>
+<?php
+    $this->load->view('public/_includes/slider');
+?>
  
-<div class="home-page-section-1">
-	<div id="featured">
-		<div>
-			<div class="section-1">
-				<h2>Welcome to Alphamed Diagnostic Center</h2>
-			</div>
-			<div class="home-content">
-				<h4>Trust the FASTEST growing diagnostic laboratory in Pampanga!</h4>
-				<h4>With 3 conveniently located branches and soon to open in Mabalacat</h4>
-					<ul>
-				      	<li>No need to wait for your results, we will email them directly to your doctor or to you, or download directly from our website</li>
-					</ul>
-				 <h4>Value for Money</h4>
-				 	<ul>
-				     	<li>Avail of our Loyalty card and get great discounts and freebies!</li>
-				     	<li>Get free ECG!</li>
-					</ul>
-				<h4>We care for you</h4>	
-					<ul>
-				     	<li>Enjoy free coffee and biscuits after blood extraction</li>
-					</ul>	
-			</div>		
-		</div>	
-	</div>
-</div>
-<div class="home-page-section-2">
-    <div class="container">
+<div class="home-content">
+    <div class="container-fluid">
         <div class="row">
             <div class="col-md-3">
-                <div class="fb-page" data-href="https://www.facebook.com/alphameddiagnosticlaboratory/" data-tabs="timeline" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
-                    <blockquote cite="https://www.facebook.com/alphameddiagnosticlaboratory/" class="fb-xfbml-parse-ignore">
-                        <a href="https://www.facebook.com/alphameddiagnosticlaboratory/">Alphamed Diagnostic Laboratory</a>
-                    </blockquote>
+               <img src="<?php echo base_url('assets/images/alphamed-slider3.jpg'); ?>"  style="height: 300px; width: 100%; padding: 20px;" alt="...">
+               <img alt="140x140" data-src="holder.js/140x140" class="img-rounded" style="width: 100%; height: 300px; padding: 20px;" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgdmlld0JveD0iMCAwIDE0MCAxNDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzE0MHgxNDAKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNWZjNGE0ODE3YyB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1ZmM0YTQ4MTdjIj48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjQzLjUiIHk9Ijc0LjgiPjE0MHgxNDA8L3RleHQ+PC9nPjwvZz48L3N2Zz4=" data-holder-rendered="true">
+            </div>
+            <div class="col-md-9">             
+                <h2>Welcome to Alphamed Diagnostic Center</h2>
+                <div class="home-content-1">
+                    <h4>Trust the FASTEST growing diagnostic laboratory in Pampanga!</h4>
+                    <h4>With 3 conveniently located branches and soon to open in Mabalacat</h4>
+                        <ul>
+                            <li>No need to wait for your results, we will email them directly to your doctor or to you, or download directly from our website</li>
+                        </ul>
+                     <h4>Value for Money</h4>
+                        <ul>
+                            <li>Avail of our Loyalty card and get great discounts and freebies!</li>
+                            <li>Get free ECG!</li>
+                        </ul>
+                    <h4>We care for you</h4>    
+                        <ul>
+                            <li>Enjoy free coffee and biscuits after blood extraction</li>
+                        </ul>    
                 </div>
-            </div>  
+               
+            <!-- </div>
             <div class="col-md-3">
+            </div> -->
+        </div>
+    </div>
+</div>
+
+<div class="home-content-2">
+    <div class="container">
+        <div class="row"> 
+            <div class="col-md-4">
                 <div class="panel panel-primary">
                     <div class="panel-heading">
                         <h3 class="panel-title">Laboratory</h3>
@@ -139,7 +87,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-4">
                 <div class="panel panel-primary">
                     <div class="panel-heading">
                         <h3 class="panel-title">Imaging</h3>
@@ -174,7 +122,7 @@
                     </div>
                 </div> 
             </div>
-            <div class="col-md-3">
+            <div class="col-md-4">
                 <div class="panel panel-primary">
                     <div class="panel-heading">
                         <h3 class="panel-title">Special Tests</h3>
@@ -220,9 +168,7 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-md-3">
-            </div>
-            <div class="col-md-3">
+            <div class="col-md-4">
                 <div class="panel panel-primary">
                     <div class="panel-heading">
                         <h3 class="panel-title">Research and Clinical</h3>
@@ -232,7 +178,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-3">
+            <div class="col-md-4">
                 <div class="panel panel-primary">
                     <div class="panel-heading">
                         <h3 class="panel-title">Other Services</h3>
@@ -262,7 +208,7 @@
                     </div>
                 </div> 
             </div>
-            <div class="col-md-3">
+            <div class="col-md-4">
                 <div class="panel panel-primary">
                     <div class="panel-heading">
                         <h3 class="panel-title">Test Packages </h3>
@@ -277,8 +223,8 @@
         <p>Note: We accept requests for examinations
     not available in the Philippines.</p>
     </div>
-    
-
+  
+</div>
  
 
 <div id="fb-root"></div>
