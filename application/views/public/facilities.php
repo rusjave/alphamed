f732678fb2b1a5eb99239dc5a57596287e408292
rusjave <rj@realestate.ph>
2017-11-19 22:36:37 +0800

    Update location and facilities

--- application/views/public/facilities.php
+++ application/views/public/facilities.php
@@ -1,30 +1,27 @@
 <?php
 	$this->load->view('public/_includes/header');
 ?>
-<div class="facilities-section">
-	<div id="body">
-		<h2 style="color: #97aeb3;
-font-size: 24px;
-font-weight: normal;
-margin: 0;
-padding: 20px 0;
-text-transform: capitalize;
-text-align: center;">Facilities</h2>
-		<ul>
-			<li>
-				<div class="figure">
-					 <img src="<?php echo base_url('/assets/images/lab-1.png'); ?>" alt="">
-				</div>
-				<div class="article">
-					<h2 style="color: #94acb3;
-					font-size: 16px;
-					display: block;
-					margin: 0;
-					padding: 0px 0 12px;
-					text-transform: capitalize;">
-						Biosystems A15 Fully AutomatedChemistry Analyzer
-					</h2>
-					<ul>
+
+<?php
+    $this->load->view('public/_includes/slider');
+?>
+
+<div class="facilities-content">
+    <div class="container-fluid">
+        <div class="row">
+        	<h2>Facilities</h2>
+            <div class="col-md-3">
+               <img src="<?php echo base_url('assets/images/alphamed-slider3.jpg'); ?>"  style="height: 300px; width: 100%; padding: 20px;" alt="...">
+               <img alt="140x140" data-src="holder.js/140x140" class="img-rounded" style="width: 100%; height: 300px; padding: 20px;" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgdmlld0JveD0iMCAwIDE0MCAxNDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzE0MHgxNDAKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNWZjNGE0ODE3YyB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1ZmM0YTQ4MTdjIj48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjQzLjUiIHk9Ijc0LjgiPjE0MHgxNDA8L3RleHQ+PC9nPjwvZz48L3N2Zz4=" data-holder-rendered="true">
+            </div>
+            <div class="col-md-2">             
+			 	<img src="<?php echo base_url('/assets/images/lab-1.png'); ?>" alt="">
+			 	<img src="<?php echo base_url('/assets/images/lab-2.png'); ?>" alt="">
+				<img src="<?php echo base_url('/assets/images/lab-3.png'); ?>" alt="">
+            </div>
+            <div class="col-md-7">
+            	<h4>BIOSYSTEMS A15 FULLY AUTOMATED CHEMISTRY ANALYZER</h4>
+					<ul class="list-unstyled">
 						<li>
 							<span>Capable of processing </span>
 						</li>
@@ -35,22 +32,9 @@ text-align: center;">Facilities</h2>
 							<span>results ready in minutes</span>
 						</li>
 					</ul>
-				</div>
-			</li>
-			<li>
-				<div class="figure">
-					 <img src="<?php echo base_url('/assets/images/lab-2.png'); ?>" alt="">
-				</div>
-				<div class="article">
-					<h2 style="color: #94acb3;
-					font-size: 16px;
-					display: block;
-					margin: 0;
-					padding: 0px 0 12px;
-					text-transform: capitalize;">
-						PHILIPS CLEARVIEW 550 
-					</h2>
-					<ul>
+					<hr>
+				<h4>PHILIPS CLEARVIEW 550 </h4>
+					<ul class="list-unstyled">
 						<li>
 							<span>For ultra fast and accurate</span>
 						</li>
@@ -58,22 +42,9 @@ text-align: center;">Facilities</h2>
 							<span>hematology test</span>						
 						</li>
 					</ul>
-				</div>
-			</li>
-			<li>
-				<div class="figure">
-					 <img src="<?php echo base_url('/assets/images/lab-3.png'); ?>" alt="">
-				</div>
-				<div class="article">
-					<h2 style="color: #94acb3;
-					font-size: 16px;
-					display: block;
-					margin: 0;
-					padding: 0px 0 12px;
-					text-transform: capitalize;">
-						2DECHO W/ DOPPLER AND GENERAL ULTRASOUND
-					</h2>
-					<ul>
+					<hr>
+				<h4>2DECHO W/ DOPPLER AND GENERAL ULTRASOUND</h4>
+					<ul class="list-unstyled">
 						<li>
 							<span>USA made with high resolution images</span>
 						</li>
@@ -81,12 +52,23 @@ text-align: center;">Facilities</h2>
 							<span>resolution graphics</span>						
 						</li>
 					</ul>
-				</div>
-			</li>
-		</ul>
-	</div>
 
+				<hr>
+            </div>
+    </div>
 </div>
+
+
+<div id="fb-root"></div>
+    <script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11';
+        fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+</script>
+
 	
 <?php
 	$this->load->view('public/_includes/footer');
--- application/views/public/locations.php
+++ application/views/public/locations.php
@@ -2,56 +2,59 @@
 	$this->load->view('public/_includes/header');
 ?>
 
-<div class="our-locations-content">
-	<div id="body">
-		<div class="content">
-			<h2>3 BRANCHES NOW OPEN TO SERVE YOU:</h2>	
-		</div>
-		<div class="row">
-			<div class="col-md-2">
-				
-			</div>
+<?php
+    $this->load->view('public/_includes/slider');
+?>
+
+<div class="services-content">
+	<div class="content">
+		<h2>3 BRANCHES NOW OPEN TO SERVE YOU:</h2>	
+	</div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3">
+               <img src="<?php echo base_url('assets/images/alphamed-slider3.jpg'); ?>"  style="height: 300px; width: 100%; padding: 20px;" alt="...">
+               <img alt="140x140" data-src="holder.js/140x140" class="img-rounded" style="width: 100%; height: 900px; padding: 20px;" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgdmlld0JveD0iMCAwIDE0MCAxNDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzE0MHgxNDAKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNWZjNGE0ODE3YyB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1ZmM0YTQ4MTdjIj48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjQzLjUiIHk9Ijc0LjgiPjE0MHgxNDA8L3RleHQ+PC9nPjwvZz48L3N2Zz4=" data-holder-rendered="true">
+            </div>
 			<div class="col-md-2">
 				<img alt="140x140" data-src="holder.js/140x140" class="img-rounded pull-right" style="width: 140px; height: 140px;" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgdmlld0JveD0iMCAwIDE0MCAxNDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzE0MHgxNDAKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNWZhMzUyZTZmMCB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1ZmEzNTJlNmYwIj48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjQzLjUiIHk9Ijc0LjgiPjE0MHgxNDA8L3RleHQ+PC9nPjwvZz48L3N2Zz4=" data-holder-rendered="true">
 				<p><a href="https://www.google.com.ph/maps/search/Unit+102+Citicorp+Business+Center+San+Isidro/@15.048483,120.6460772,15z/data=!3m1!4b1?hl=en">View Roadmap</a></p>
 			</div>
-			<div class="col-md-8">
+			<div class="col-md-7">
 				<h4>San Fernando Main Branch</h4>
 				<div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=Unit 102 Citicorp Business Center San Isidro&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="http://www.embedgooglemap.net">embed google map</a></div><style>.mapouter{overflow:hidden;height:500px;width:600px;}.gmap_canvas {background:none!important;height:500px;width:600px;}</style></div>
-			</div>
-		</div>
-		<hr>
-		<div class="row">
-			<div class="col-md-2">
-				
 			</div>
 			<div class="col-md-2">
 				<img alt="140x140" data-src="holder.js/140x140" class="img-rounded pull-right" style="width: 140px; height: 140px;" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgdmlld0JveD0iMCAwIDE0MCAxNDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzE0MHgxNDAKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNWZhMzUyZTZmMCB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1ZmEzNTJlNmYwIj48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjQzLjUiIHk9Ijc0LjgiPjE0MHgxNDA8L3RleHQ+PC9nPjwvZz48L3N2Zz4=" data-holder-rendered="true">
 				<p><a href="https://www.google.com.ph/maps/place/V.L.+Makabali+Memorial+Hospital/@15.0318433,120.6867064,17z/data=!3m1!4b1!4m5!3m4!1s0x3396f70b4cefcb39:0x683cea7ddfa49793!8m2!3d15.0318381!4d120.6888951?hl=en">View Roadmap</a></p>
 			</div>
-			<div class="col-md-8">
+			<div class="col-md-7">
 				<h4>Makabali</h4>
 				<div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=B. Mendoza St. CSFP Makabali Pamapnga&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="http://www.embedgooglemap.net">embed google map</a></div><style>.mapouter{overflow:hidden;height:500px;width:600px;}.gmap_canvas {background:none!important;height:500px;width:600px;}</style></div>
-			</div>
-		</div>
-		<hr>
-		<div class="row">
-			<div class="col-md-2">
-				
 			</div>
 			<div class="col-md-2">
 				<img alt="140x140" data-src="holder.js/140x140" class="img-rounded pull-right" style="width: 140px; height: 140px;" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgdmlld0JveD0iMCAwIDE0MCAxNDAiIHByZXNlcnZlQXNwZWN0UmF0aW89Im5vbmUiPjwhLS0KU291cmNlIFVSTDogaG9sZGVyLmpzLzE0MHgxNDAKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNWZhMzUyZTZmMCB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1ZmEzNTJlNmYwIj48cmVjdCB3aWR0aD0iMTQwIiBoZWlnaHQ9IjE0MCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjQzLjUiIHk9Ijc0LjgiPjE0MHgxNDA8L3RleHQ+PC9nPjwvZz48L3N2Zz4=" data-holder-rendered="true">
 				<p><a href="https://www.google.com.ph/maps/search/JP+Rizal+St.+Parian+Mexico+Pampanga/@15.0646086,120.718276,17z/data=!3m1!4b1?hl=en">View Roadmap</a></p>
+
 			</div>
-			<div class="col-md-8">
+			<div class="col-md-7">
 				<h4>Mexico</h4>
 				<div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=JP Rizal St. Parian Mexico Pampanga&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="http://www.embedgooglemap.net">embed google map</a></div><style>.mapouter{overflow:hidden;height:500px;width:600px;}.gmap_canvas {background:none!important;height:500px;width:600px;}</style></div>
 			</div>
-		</div>
-		<hr>
-	</div>
+        </div>
+    </div>
 </div>
 
+
+<div id="fb-root"></div>
+    <script>(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11';
+        fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+</script>
 
 <?php
 	$this->load->view('public/_includes/footer');
